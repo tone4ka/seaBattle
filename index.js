8496716a288614127b8b852de17b193821ed0136
Antonina Zinkevich <antonina_zinkevich@epam.com>
2021-08-17 18:55:12 +0300

    session add

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const exphbs = require("express-handlebars");
 const path = require('path');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
+const session = require("express-session");
+const MongoStore = require("connect-mongodb-session")(session);
 const usersRoutes = require('./routes/users');
-const registrationRoutes = require('./routes/registration');
 const homeRoutes = require('./routes/home');
+const authRoutes = require('./routes/auth');
 const User = require('./models/user');
-
+const varMiddleware = require('./middleware/variables');
+const MONGODB_URI = 'mongodb+srv://tonia2:VikpBhS70Tmw3hqP@cluster0.wdbfb.mongodb.net/seabattle?retryWrites=true&w=majority';
 
 const {
     allowInsecurePrototypeAccess,
@@ -24,27 +27,34 @@ app.engine("hbs", hbs.engine); //регистрируем движок
 app.set("view engine", "hbs"); //начинаем использовать движок(регистрируем)
 app.set("views", "views"); //конфигурируем переменную, где будут храниться наши шаблоны(папка views)
 app.use(express.static(path.join(__dirname, "public")));//зарегистрировали папку паблик по умолчанию
+app.use(express.urlencoded({ extended: true }));
+const store = new MongoStore({
+  collection: "sessions",
+  uri: MONGODB_URI,
+});
+app.use(
+  session({
+    secret: 'SESSION SECRET',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(varMiddleware);
+
 app.use('/', homeRoutes);
 app.use('/users', usersRoutes);
-app.use('/registration', registrationRoutes);
+app.use('/auth', authRoutes);
 
 async function start() {
   try {
-    const url = 'mongodb+srv://tonia2:VikpBhS70Tmw3hqP@cluster0.wdbfb.mongodb.net/seabattle?retryWrites=true&w=majority';
-    console.log('tonia2:VikpBhS70Tmw3hqP');
+    const url = MONGODB_URI;
     await mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
     });
-    const candidate = await User.findOne(); // без параметров находим есть ли там хотя бы один пользователь(в базе данных)
-    if(!candidate) {
-      const user = new User({
-        email: 'vasia@gmail.com',
-        name: 'vasia'
-      })
-      await user.save();
-    }
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
