6848d1aa5a693134e81e0d96e42477e015f4cb70
Antonina Zinkevich <antonina_zinkevich@epam.com>
2021-10-27 15:24:30 +0300

    index cleaning

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import express from "express";
-import Handlebars from "handlebars";
 import exphbs from "express-handlebars";
 import path from "path";
 import csrf from "csurf";
-import flash from "connect-flash"; //для передачи данных через сессию
+import flash from "connect-flash";
 import mongoose from "mongoose";
 const PORT = process.env.PORT || 3000;
 import session from "express-session";
@@ -12,25 +11,21 @@ let MongoStoreSession = MongoStore(session);
 import onTheSeaRoutes from "./routes/onTheSea.js";
 import homeRoutes from "./routes/home.js";
 import authRoutes from "./routes/auth.js";
-import keys from "./keys/index.js"; //сюда вынесли переменные
+import keys from "./keys/index.js";
 import varMiddleware from "./middleware/variables.js";
 import usersMiddleware from "./middleware/usersArr.js";
 const app = express();
 import runSocketIo from "./websocket/runSocketIo.js";
 
-// const {
-//   allowInsecurePrototypeAccess,
-// } = require("@handlebars/allow-prototype-access");
 const hbs = exphbs.create({
   defaultLayout: "main",
-  extname: "hbs", //чтобы каждый раз не писать handlebars
-  // handlebars: allowInsecurePrototypeAccess(Handlebars), //это чтобы отключить проверку для отображения картинок
+  extname: "hbs",
 });
 
-app.engine("hbs", hbs.engine); //регистрируем движок
-app.set("view engine", "hbs"); //начинаем использовать движок(регистрируем)
-app.set("views", "views"); //конфигурируем переменную, где будут храниться наши шаблоны(папка views)
-app.use(express.static(path.join(path.resolve(path.dirname('')), "public"))); //зарегистрировали папку паблик по умолчанию
+app.engine("hbs", hbs.engine); 
+app.set("view engine", "hbs"); 
+app.set("views", "views"); 
+app.use(express.static(path.join(path.resolve(path.dirname('')), "public")));
 app.use(express.urlencoded({ extended: true }));
 const store = new MongoStoreSession({
   collection: "sessions",
